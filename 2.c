32da4dc5aedf9da52a619d1d7f56eb2a76a9ae57
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-12-19 11:13:46 +0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+int main()
+{
+    char *a = "qwer2331";
+    char *b = "qwerabcd";
+    char c[10], d[10];
+    printf("%s\n%s\n", a, b);
+    printf("identical >> %d\n", compare(a, b));
+    printf("remaining after identical:\na >> %d\nb >> %d\n\n", len(a) - compare(a, b), len(b) - compare(a, b));
+    deltacopy(a, b, c);//Подсчет количество символов, остающихся после последнего совпадения в каждой из строк
+    mixcopy(a, b, d);
+    printf("delta >> %s\nmix >> %s\n", c, d);
+}
+
 int compare(char *a, char *b)
 {
     int len = 0;
@@ -10,7 +23,7 @@ int compare(char *a, char *b)
         else
             return len;
     }
-    return len;
+    return len; // Проверка, сколько первых символов в строках совпадают.
 }
 
 int len(char *s)
@@ -21,7 +34,7 @@ int len(char *s)
     return s - c - 1;
 }
 
-void deltacopy(char *a, char *b, char *c)
+void deltacopy(char *a, char *b, char *c)//Составление новой строки(начать символами первой строки, а продолжить из второй)
 {
     int idc = compare(a, b);
     a += idc;
@@ -32,7 +45,7 @@ void deltacopy(char *a, char *b, char *c)
         *(c++) = *b;
 }
 
-void mixcopy(char *a, char *b, char *c)
+void mixcopy(char *a, char *b, char *c)//Составление новой строки(начать с первого символа первой строки, затем первый символ втоpой, второй символ первой и т.д.)
 {
     int idc = compare(a, b);
     a += idc;
@@ -42,17 +55,4 @@ void mixcopy(char *a, char *b, char *c)
         *(c++) = *a;
         *(c++) = *b;
     }
-}
-
-int main()
-{
-    char *a = "abcd1234";
-    char *b = "abcdefgh";
-    char c[10], d[10];
-    printf("%s\n%s\n", a, b);
-    printf("identical >> %d\n", compare(a, b));
-    printf("remaining after identical:\na >> %d\nb >> %d\n\n", len(a) - compare(a, b), len(b) - compare(a, b));
-    deltacopy(a, b, c);
-    mixcopy(a, b, d);
-    printf("delta >> %s\nmix >> %s\n", c, d);
 }
